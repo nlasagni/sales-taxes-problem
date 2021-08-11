4db060e533b4da5270b6656faf12e9b1ab852b59
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-08-11 16:57:14 +0200

    Introduces UseCaseInput port

---
+++ src/main/kotlin/usecase/UseCaseInput.kt
@@ -0,0 +1,15 @@
+package usecase
+
+/**
+ * The input port of the Use Case layer.
+ *
+ * @author Nicola Lasagni on 11/08/2021.
+ */
+interface UseCaseInput<T> {
+
+    /**
+     * Executes the task related to the [request] specified.
+     */
+    fun execute(request: T)
+
+}
