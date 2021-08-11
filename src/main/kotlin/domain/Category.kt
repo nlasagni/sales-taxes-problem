28ecf0a3291db51cd1ab62e15d95a8d865100fc4
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-08-11 13:31:25 +0200

    Adds essential entities

---
+++ src/main/kotlin/domain/Category.kt
@@ -0,0 +1,13 @@
+package domain
+
+/**
+ * The category of an [Item].
+ *
+ * @author Nicola Lasagni on 11/08/2021.
+ */
+enum class Category {
+    BOOK,
+    FOOD,
+    MEDICAL,
+    MISCELLANEOUS
+}
---
+++ src/main/kotlin/domain/Item.kt
@@ -0,0 +1,14 @@
+package domain
+
+/**
+ * An Item that can be purchased.
+ *
+ * @author Nicola Lasagni on 11/08/2021.
+ */
+data class Item(
+    val name: String,
+    val category: Category,
+    val quantity: Int,
+    val unitPrice: Float,
+    val imported: Boolean
+)
