103d90799c29755d42a35fd184a4506ebb8a7b49
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-08-12 03:27:17 +0200

    Adds application plugin and set main class name in order to execute the application

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("pl.droidsonroids.jacoco.testkit") version "1.0.7"
     id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
     id("io.gitlab.arturbosch.detekt") version "1.14.1"
+    application
 }
 
 repositories {
@@ -16,6 +17,10 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion") // for kotest framework
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion") // for kotest core assertions
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion") // for kotest core jvm assertions
+}
+
+application {
+    mainClass.set("SalesTaxesApplicationKt")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
